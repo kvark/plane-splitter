a0d00b8e805305faf4fdf1743c47d634536fd315
Dzmitry Malyshau <kvark@mozilla.com>
2018-09-19 12:45:55 -0400

    Update to latest dependencies fetched from crates.io

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate cgmath;
 extern crate euclid;
 extern crate env_logger;
+extern crate gfx;
 extern crate mint;
 extern crate plane_split;
 extern crate ron;
@@ -13,25 +14,29 @@ use std::fs::File;
 use std::time::SystemTime;
 
 use cgmath::prelude::*;
-use three::{GfxPrimitive, gfx_state as state, gfx_preset as preset};
+use gfx::state::{
+    Blend, BlendChannel, Comparison, ColorMask, Equation, Factor,
+    Rasterizer, Stencil, StencilOp, StencilSide,
+};
 use plane_split::Splitter;
+use three::Object;
 
 
-const STENCIL_PASS: state::StencilSide = state::StencilSide {
-    fun: state::Comparison::Always,
+const STENCIL_PASS: StencilSide = StencilSide {
+    fun: Comparison::Always,
     mask_read: 0,
     mask_write: 0,
-    op_fail: state::StencilOp::Keep,
-    op_depth_fail: state::StencilOp::Keep,
-    op_pass: state::StencilOp::Keep,
+    op_fail: StencilOp::Keep,
+    op_depth_fail: StencilOp::Keep,
+    op_pass: StencilOp::Keep,
 };
-const STENCIL_SET: state::StencilSide = state::StencilSide {
-    fun: state::Comparison::Equal,
+const STENCIL_SET: StencilSide = StencilSide {
+    fun: Comparison::Equal,
     mask_read: !0,
     mask_write: !0,
-    op_fail: state::StencilOp::Keep,
-    op_depth_fail: state::StencilOp::Keep,
-    op_pass: state::StencilOp::IncrementClamp,
+    op_fail: StencilOp::Keep,
+    op_depth_fail: StencilOp::Keep,
+    op_pass: StencilOp::IncrementClamp,
 };
 
 
@@ -43,46 +48,69 @@ struct Plane {
 }
 
 fn main() {
-    env_logger::init().unwrap();
+    env_logger::init();
 
-    let mut win = three::Window::new("Plane splitter", "../three-rs/data/shaders").build();
-    let mut cam = win.factory.perspective_camera(60.0, 1.0, 10.0);
-    let mut controls = three::OrbitControls::new(&cam, [0.0, 1.0, 3.0], [0.0, 0.0, 0.0]).build();
+    let mut win = three::Window::new("Plane Splitter");
+    let cam = win.factory.perspective_camera(60.0, 1.0 .. 10.0);
+    let mut controls = three::controls::Orbit::builder(&cam)
+        .position([0.0, 1.0, 3.0])
+        .target([0.0, 0.0, 0.0])
+        .build();
     win.scene.add(&cam);
 
-    let pipeline_base = win.factory.basic_pipeline("data/poly",
-        GfxPrimitive::TriangleList,
-        state::Rasterizer::new_fill(),
-        state::MASK_ALL,
-        preset::blend::REPLACE,
-        preset::depth::LESS_EQUAL_WRITE,
-        state::Stencil { front: STENCIL_PASS, back: STENCIL_PASS, },
+    let pipeline_base = win.factory.basic_pipeline(
+        "data",
+        "poly",
+        gfx::Primitive::TriangleList,
+        Rasterizer::new_fill(),
+        ColorMask::all(),
+        gfx::preset::blend::REPLACE,
+        gfx::preset::depth::LESS_EQUAL_WRITE,
+        Stencil { front: STENCIL_PASS, back: STENCIL_PASS, },
         ).unwrap();
-    let pipeline_a = win.factory.basic_pipeline("data/poly",
-        GfxPrimitive::TriangleList,
-        state::Rasterizer::new_fill(),
-        state::RED | state::GREEN,
-        state::Blend { color: state::BlendChannel { equation: state::Equation::Sub, source: state::Factor::One, destination: state::Factor::One }, .. preset::blend::REPLACE },
-        preset::depth::PASS_TEST,
-        state::Stencil { front: STENCIL_SET, back: STENCIL_SET, },
+    let pipeline_a = win.factory.basic_pipeline(
+        "data",
+        "poly",
+        gfx::Primitive::TriangleList,
+        Rasterizer::new_fill(),
+        ColorMask::RED | ColorMask::GREEN,
+        Blend {
+            color: BlendChannel {
+                equation: Equation::Sub,
+                source: Factor::One,
+                destination: Factor::One,
+            },
+            .. gfx::preset::blend::REPLACE
+        },
+        gfx::preset::depth::PASS_TEST,
+        Stencil { front: STENCIL_SET, back: STENCIL_SET, },
         ).unwrap();
-    let pipeline_b = win.factory.basic_pipeline("data/poly",
-        GfxPrimitive::TriangleList,
-        state::Rasterizer::new_fill(),
-        state::RED | state::GREEN,
-        state::Blend { color: state::BlendChannel { equation: state::Equation::RevSub, source: state::Factor::One, destination: state::Factor::One }, .. preset::blend::REPLACE },
-        preset::depth::PASS_TEST,
-        state::Stencil { front: STENCIL_SET, back: STENCIL_SET, },
+    let pipeline_b = win.factory.basic_pipeline(
+        "data",
+        "poly",
+        gfx::Primitive::TriangleList,
+        Rasterizer::new_fill(),
+        ColorMask::RED | ColorMask::GREEN,
+        Blend {
+            color: BlendChannel {
+                equation: Equation::RevSub,
+                source: Factor::One,
+                destination: Factor::One,
+            },
+            .. gfx::preset::blend::REPLACE
+        },
+        gfx::preset::depth::PASS_TEST,
+        Stencil { front: STENCIL_SET, back: STENCIL_SET, },
         ).unwrap();
 
-    let geometry = three::Geometry::new_plane(2.0, 2.0);
+    let geometry = three::Geometry::plane(2.0, 2.0);
     let mut last_time = SystemTime::now();
     let mut file = File::open("data/poly.ron").expect("Unable to open scene description");
     let mut splitter = plane_split::BspSplitter::<f32, ()>::new();
     let mut meshes = Vec::new();
     let mut frame_id = 0usize;
 
-    while win.update() && !three::KEY_ESCAPE.is_hit(&win.input) {
+    while win.update() && !win.input.hit(three::KEY_ESCAPE) {
         let write_time = file.metadata().unwrap().modified().unwrap();
         if write_time != last_time {
             last_time = write_time;
@@ -105,7 +133,7 @@ fn main() {
 
             for (i, plane) in planes.iter().enumerate() {
                 let gray = (i + 1) * 0xFF / planes.len();
-                let mat = three::Material::CustomBasicPipeline {
+                let mat = three::material::basic::Custom {
                     color: (gray as u32) * 0x010101,
                     map: None,
                     pipeline: pipeline_base.clone(),
@@ -124,23 +152,29 @@ fn main() {
                     scale: plane.scale,
                 };
                 let transform = euclid::TypedTransform3D::from_row_arrays(cgmath::Matrix4::from(decomposed).into());
-                let poly = plane_split::Polygon::from_transformed_rect(rect, transform, i);
-                splitter.add(poly);
+                if let Some(poly) = plane_split::Polygon::from_transformed_rect(rect, transform, i) {
+                    splitter.add(poly);
+                }
             }
         }
 
         let mut points = Vec::new();
         let mut temp = Vec::new();
-        let view_dir = {
-            let node = cam.sync(&win.scene);
-            let dir = cgmath::Quaternion::from(node.world_transform.orientation) *
-                cgmath::Vector3::unit_z();
-            euclid::TypedVector3D::new(dir.x, dir.y, dir.z)
+        let view_direction = {
+            let node = win.scene
+                .sync_guard()
+                .resolve_world(&cam);
+            let dir: [f32; 3] = cgmath::Quaternion::from(node.transform.orientation)
+                .rotate_vector(cgmath::Vector3::unit_z())
+                .into();
+            euclid::TypedVector3D::from(dir)
         };
 
+        //println!("{:?}", splitter);
+
         //reverse the order to draw front to back
-        let results = splitter.sort(view_dir * -1.0);
-        for poly in results {
+        let results = splitter.sort(view_direction);
+        for poly in results.iter().rev() {
             points.clear();
             for &k in &[0,1,2,2,3,0] {
                 let p = &poly.points[k];
@@ -148,9 +182,9 @@ fn main() {
             }
             let start = points[0].clone();
             points.push(start);
-            let geom = three::Geometry::from_vertices(points.clone());
+            let geom = three::Geometry::with_vertices(points.clone());
             let gray = (poly.anchor + 1) * 0xFF / meshes.len();
-            let mat = three::Material::CustomBasicPipeline {
+            let mat = three::material::basic::Custom {
                 color: (gray as u32) * 0x010101,
                 map: None,
                 pipeline: if frame_id & 1 == 0 {
@@ -165,7 +199,7 @@ fn main() {
         }
 
         temp.reverse(); //HACK: ensure drop order
-        if three::KEY_SPACE.is_hit(&win.input) {
+        if win.input.hit(three::KEY_SPACE) {
             println!("{:#?}", results);
         }
         frame_id += 1;
